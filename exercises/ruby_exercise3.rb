edea7b447506f61734629554feb06aaa7b7cfe31
ps7duck <daksamit@daksamit.pl>
2016-07-18 12:41:12 +0200

    exercise 3 done

--- exercises/ruby_exercise3.rb
+++ exercises/ruby_exercise3.rb
@@ -1,6 +1,14 @@
 module RubyExercise3
 
   def self.hashify(keys, values)
+    result = {}
+    i=0
+    raise "not matching array sizes are provided" if keys.size != values.size
+    while i < keys.size
+      result[keys[i]] = values[i]
+      i=i+1
+    end
+    result
   end
 
 end
