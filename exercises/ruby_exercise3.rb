20f901335ab4c32d2ec17497965245cd8dbc9abb
ps7duck <daksamit@daksamit.pl>
2016-07-18 12:35:55 +0200

    exercise 3 done

--- exercises/ruby_exercise3.rb
+++ exercises/ruby_exercise3.rb
@@ -1,9 +1,13 @@
 module RubyExercise3
 
   def self.to_camel_case(string)
+    output = string.split('_').map{|exp| exp.capitalize}.join
+    output[0] = output[0].downcase
+    output
   end
 
   def self.to_capital_case(string)
+    string.split('_').map{|e| e.capitalize}.join
   end
 
 end
