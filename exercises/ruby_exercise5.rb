db619e5000b2a7c8362b2d67e3dd47523e2e7783
ps7duck <daksamit@daksamit.pl>
2016-07-18 12:29:35 +0200

    exercise 5 done

--- exercises/ruby_exercise5.rb
+++ exercises/ruby_exercise5.rb
@@ -1,15 +1,21 @@
 class TrueClass
   def if_true
+    yield
+    self
   end
 
   def if_false
+    self
   end
 end
 
 class FalseClass
   def if_true
+    self
   end
 
   def if_false
+    yield
+    self
   end
 end
