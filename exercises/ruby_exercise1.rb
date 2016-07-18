1f356e7c5122b754e10cb00e904d0840c17a4ec6
ps7duck <daksamit@daksamit.pl>
2016-07-18 12:33:44 +0200

    exercise 1 done

--- exercises/ruby_exercise1.rb
+++ exercises/ruby_exercise1.rb
@@ -1,6 +1,19 @@
 module RubyExercise1
 
-  def self.letter_occurance_check
+  def self.letter_occurance_check(string)
+    result = '00000000000000000000000000'
+    if string.to_s == ''
+      result
+    else
+      string.split('').each do |c|
+        i = c.ord
+        if i.between?(65,90) || i.between?(97,122)
+          i = i-32 if i.between?(97,122)
+          result[i-65] = '1'
+        end
+      end
+      result
+    end
   end
 
 end
