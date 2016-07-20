0122e1740c62142dfaec246a678b45c9d5bbffc4
ps7duck <daksamit@daksamit.pl>
2016-07-20 10:52:46 +0200

    exercise 1 done

--- exercises/ruby_exercise1.rb
+++ exercises/ruby_exercise1.rb
@@ -1,19 +1,7 @@
 module RubyExercise1
 
-  def self.letter_occurance_check(string)
-    result = '00000000000000000000000000'
-    if string.to_s == ''
-      result
-    else
-      string.split('').each do |c|
-        i = c.ord
-        if i.between?(65,90) || i.between?(97,122)
-          i = i-32 if i.between?(97,122)
-          result[i-65] = '1'
-        end
-      end
-      result
-    end
+  def self.letter_occurance_check(string = "")
+    ('a'..'z').map { |letter| string.match(/#{letter}/i) ? "1" : "0"}.join
   end
 
 end
