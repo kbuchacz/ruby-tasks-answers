f183e295a03a5420f5602aff5f3b63e7bde69dac
Dominik Aksamit <daksamit@daksamit.pl>
2016-07-27 11:55:11 +0200

    exercise 1 done

--- exercises/ruby_exercise1.rb
+++ exercises/ruby_exercise1.rb
@@ -1,9 +1,6 @@
 module RubyExercise1
 
   def self.valid_email?(email)
-    result = false
-    result = true if email =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-    result
+    email =~ /\A([\w+\-]\.?)+\w@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   end
-
 end
